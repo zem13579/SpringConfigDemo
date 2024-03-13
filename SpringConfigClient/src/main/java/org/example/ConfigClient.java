aa55c5ec53655b27599bdf7b43cea8b2617134c7
zem <zhangenmingy@foxmail.com>
2024-03-13 23:17:00 +0800

    demo

--- SpringConfigClient/src/main/java/org/example/ConfigClient.java
+++ SpringConfigClient/src/main/java/org/example/ConfigClient.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class ConfigClient {
-    
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/")
+    public String home() {
+        return profile;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ConfigClient.class, args);
     }
 }
--- SpringConfigServer/src/main/java/org/example/ConfigServer.java
+++ SpringConfigServer/src/main/java/org/example/ConfigServer.java
@@ -1,9 +1,9 @@
 package org.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+@SpringBootApplication
 @EnableConfigServer
 public class ConfigServer {
     public static void main(String[] args) {
