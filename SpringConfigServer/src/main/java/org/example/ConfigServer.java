7ae0502a47d487aa451ce26606f3cc70d893226a
zem <zhangenmingy@foxmail.com>
2024-03-14 14:52:32 +0800

    commit

--- SpringConfigServer/src/main/java/org/example/ConfigServer.java
+++ SpringConfigServer/src/main/java/org/example/ConfigServer.java
@@ -1,10 +1,15 @@
 package org.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
 @EnableConfigServer
+@SpringBootApplication
+@EnableEurekaServer
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
--- SpringConfigServer/src/main/java/org/example/Configshow.java
+++ SpringConfigServer/src/main/java/org/example/Configshow.java
@@ -1,24 +1,21 @@
-package org.example.config;
+package org.example;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
  * @Describe
  * @Author zem
- * @CreateDate 2024/3/12 0012 15:01
+ * @CreateDate 2024/3/13 0013 15:06
  */
 @Component
-public class PrintConifg implements CommandLineRunner {
-    
+public class Configshow implements CommandLineRunner {
     @Value("${env.tag}")
-    private String profileEnv;
-    
+    private String env;
     
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(profileEnv);
+        System.out.println(env);
     }
 }
